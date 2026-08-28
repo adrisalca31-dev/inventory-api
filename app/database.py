15bbd2bd759e9e93c8864820dcc2f99479568182
adrisalca31-dev <adrisalca31@gmail.com>
2026-08-27 19:45:00 -0600

    Separate application configuration

--- app/database.py
+++ app/database.py
@@ -1,14 +1,8 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.models import Base
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+from app.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
